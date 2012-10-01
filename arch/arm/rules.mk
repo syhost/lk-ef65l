d34c659b48f6a2ea4e2cf09440d0c7a37540cb00
syhost <syhost@163.com>
2012-10-01 18:13:39 +0800

    arch/arm/rules.mk

--- arch/arm/rules.mk
+++ arch/arm/rules.mk
@@ -99,7 +99,8 @@ OBJS += \
 	$(LOCAL_DIR)/dcc.o
 
 # set the default toolchain to arm eabi and set a #define
-TOOLCHAIN_PREFIX ?= /usr/local/arm-eabi-4.4.3/bin/arm-eabi-
+# TOOLCHAIN_PREFIX ?= /usr/local/arm-eabi-4.4.3/bin/arm-eabi-
+TOOLCHAIN_PREFIX ?= arm-eabi-
 ifeq ($(TOOLCHAIN_PREFIX),arm-none-linux-gnueabi-)
 # XXX test for EABI better than this
 # eabi compilers dont need this
